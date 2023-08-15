79d0678982dff32b18503b12c3e02c1335456832
Luis Martinez <rust@Luiss-Mac-mini.local>
2023-08-15 00:24:51 -0600

    User table changes.

--- models.py
+++ models.py
@@ -37,7 +37,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(1000), nullable=False)
     # These will act like a List of BlogPost and Comment objects
     posts = relationship("BlogPost", back_populates="author")
